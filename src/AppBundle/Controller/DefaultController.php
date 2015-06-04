0f625bb58912abc3aff469944aee338d8a9e3a1f
zirby <capella@chzirbes.be>
2015-06-04 12:31:19 +0200

    siute copies

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,10 +8,68 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/pgi", name="pgi_homepage")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('pgi/index.html.twig');
     }
+    /**
+     * @Route("/pgi/flot", name="pgi_flot")
+     */
+    public function flotAction()
+    {
+        return $this->render('pgi/flot.html.twig');
+    }
+
+    /**
+     * @Route("/pgi/morris", name="pgi_morris")
+     */
+    public function morrisAction()
+    {
+        return $this->render('pgi/morris.html.twig');
+    }
+
+    /**
+     * @Route("/pgi/tables", name="pgi_tables")
+     */
+    public function tablesAction()
+    {
+        return $this->render('pgi/tables.html.twig');
+    }
+
+    /**
+     * @Route("/pgi/forms", name="pgi_forms")
+     */
+    public function formsAction()
+    {
+        return $this->render('pgi/forms.html.twig');
+    }
+
+    /**
+     * @Route("/pgi/panelswells", name="pgi_panelswells")
+     */
+    public function panelswellsAction()
+    {
+        return $this->render('pgi/panelswells.html.twig');
+    }
+
+    
+   /**
+     * @Route("/pgi/buttons", name="pgi_buttons")
+     */
+    public function buttonsAction()
+    {
+        return $this->render('pgi/buttons.html.twig');
+    }
+
+       /**
+     * @Route("/pgi/notifications", name="pgi_notifications")
+     */
+    public function notificationsAction()
+    {
+        return $this->render('pgi/notifications.html.twig');
+    }
+
+    
 }
